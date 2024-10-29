cd5e0e9c4e7867c16270f5b85169735d2be34e90
Clément Guillemot <guillemot.clement@protonmail.com>
2024-10-29 21:40:15 +0100

    Ajout de la dernière partie - bug incrémentation score

--- main.js
+++ main.js
@@ -38,3 +38,60 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice){
+  if(humanChoice === 'pierre'){
+    if(computerChoice === 'feuille'){
+      computerScore++;
+      return "Computer gagne";
+    }else if(computerChoice === 'ciseau'){
+      humanScore++;
+      return "Humain gagne"
+    }else{
+      return "Egalité";
+    }
+  }else if(humanChoice === 'ciseau'){
+    if(computerChoice === 'feuille'){
+      humanScore++;
+      return "Humain gagne";
+    }else if(computerChoice === 'pierre'){
+      computerScore++;
+      return "Computer gagne"
+    }else{
+      return "Egalité";
+    }
+  }else if(humanChoice === 'feuille'){
+    if(computerChoice === 'ciseau'){
+      computerScore++;
+      return "Computer gagne";
+    }else if(computerChoice === 'feuille'){
+      humanScore++;
+      return "Humain gagne"
+    }else{
+      return "Egalité";
+    }
+  }
+}
+
+function playGame(){
+  let numberRound = 5;
+  let i = 0;
+  while(i <= numberRound){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    console.log("Score humain :" + humanScore);
+    console.log('Score de Terminator :'+computerScore);
+    i++;
+    console.log('Round n° :' + i);
+  }
+
+  if(humanScore > computerScore){
+    return "Victoire de l'humain";
+  }else{
+    return "Victoire de terminator";
+  }
+}
+
+console.log(playGame());
