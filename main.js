8e4c0fb9f5d74d60b9a429d9e82b438ec4a09888
Clément Guillemot <guillemot.clement@protonmail.com>
2024-10-29 09:33:00 +0100

    Creation de la fonction getComputerChoice()

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+/**
+ *@func - La fonction retourne le choix du pc selon un nombre aléatoire
+ */
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  switch (computerChoice) {
+    case 0:
+      computerChoice = "paper";
+      break;
+    case 1:
+      computerChoice = "scissors";
+      break;
+    case 2:
+      computerChoice = "rock";
+      break;
+    default:
+      throw new Error("Erreur critique.");
+      break;
+  }
+  return computerChoice;
+}
+
+let computerChoice = getComputerChoice();
+
