2ba4a9c5a043970f3faaf51f94079120c9c15891
GuillemotClement <guillemot.clement@protonmail.com>
2024-11-01 09:39:21 +0100

    Solution finale

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function getHumanChoice(){
     let valideValue = false;
     let userChoice;
     while (!valideValue){
-        userChoice = parseInt(prompt("Saisir une valeur (0 - Pierre, 1 - Feuille, 3 - Ciseau)"));
+        userChoice = parseInt(prompt("Saisir une valeur (0 - Pierre, 1 - Feuille, 2 - Ciseau)"));
         if(userChoice === 1 || userChoice === 0 || userChoice === 2) {
             valideValue = true;
         }
@@ -41,5 +41,67 @@ function getHumanChoice(){
     return returnChoice(userChoice);
 }
 
+/**
+ * Retourne le vainqueur du round
+ * @param humanChoice
+ * @param computerChoice
+ */
+function playRound(humanChoice, computerChoice){
+    console.log("Choix de humain : " + humanChoice);
+    console.log("Choix de l'ordinateur : " + computerChoice);
+    if(humanChoice === "pierre"){
+        if(computerChoice === "feuille"){
+            computerScore++;
+            return "Victoire de l'ordinateur";
+        }else if(computerChoice === "pierre"){
+            return "Égalité";
+        }else if(computerChoice === "ciseau"){
+            humanScore++;
+            return "Victoire de l'humain";
+        }
+    }else if(humanChoice === "feuille"){
+        if(computerChoice === "feuille"){
+            return "Égalité";
+        }else if(computerChoice === "pierre"){
+            humanScore++;
+            return "Victoire de l'humain";
+        }else if(computerChoice === "ciseau"){
+            computerScore++;
+            return "Victoire de l'ordinateur";
+        }
+    }else if(humanChoice === "ciseau"){
+        if(computerChoice === "feuille"){
+            humanScore++;
+            return "Victoire de l'humain";
+        }else if(computerChoice === "pierre"){
+            computerScore++;
+            return "Victoire de l'ordinateur";
+        }else if(computerChoice === "ciseau"){
+            return "Égalité";
+        }
+    }
+}
+
+function playGame(numberOfRound){
+    let round = 1;
+
+    while (round <= numberOfRound){
+        console.log("Round n°" + round);
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        console.log("Score de l'humain :" + humanScore);
+        console.log("Score du computer :" + computerScore);
+
+        if(round === numberOfRound){
+            if(humanScore > computerScore){
+                console.log("Victoire finale de l'humain");
+            }else{
+                console.log("Victoire finale de l'ordinateur");
+            }
+        }
+        round++;
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+playGame(5);
